3ff7f68d41ebf10c846f01c4526987211214d2a4
tranquan <tranquan12a11@gmail.com>
2025-11-24 23:50:08 +0700

    WIP: Logic UI List image product

---
+++ app/src/main/java/com/datn/bia/a/present/activity/prod/ImageAdapter.kt
@@ -0,0 +1,46 @@
+package com.datn.bia.a.present.activity.prod
+
+import android.annotation.SuppressLint
+import android.content.Context
+import androidx.databinding.ViewDataBinding
+import com.bumptech.glide.Glide
+import com.datn.bia.a.R
+import com.datn.bia.a.common.base.BaseRecyclerViewAdapter
+import com.datn.bia.a.common.base.ext.click
+import com.datn.bia.a.databinding.ItemImageProductBinding
+
+class ImageAdapter(
+    private val contextParams: Context,
+    private val onItemClicked: (String) -> Unit
+): BaseRecyclerViewAdapter<String>() {
+    override fun getItemLayout(): Int = R.layout.item_image_product
+
+    @SuppressLint("NotifyDataSetChanged")
+    override fun submitData(newData: List<String>) {
+        list.apply {
+            clear()
+            addAll(newData)
+        }
+        notifyDataSetChanged()
+    }
+
+    override fun setData(
+        binding: ViewDataBinding,
+        item: String,
+        layoutPosition: Int
+    ) {
+        if (binding is ItemImageProductBinding) {
+            Glide.with(contextParams).load(item).into(binding.imgProduct)
+        }
+    }
+
+    override fun onClickViews(binding: ViewDataBinding, obj: String, layoutPosition: Int) {
+        super.onClickViews(binding, obj, layoutPosition)
+
+        if (binding is ItemImageProductBinding) {
+            binding.root.click {
+                onItemClicked.invoke(obj)
+            }
+        }
+    }
+}
