3a0ba9005a255cc5ac5242435100071f3ba8c00e
samptph48164-beep <samptph48164@fpt.edu.vn>
2025-11-24 22:39:16 +0700

    Done: Add Logic Format Number By vnd

--- app/src/main/java/com/datn/bia/a/common/base/ext/NumberExt.kt
+++ app/src/main/java/com/datn/bia/a/common/base/ext/NumberExt.kt
@@ -2,6 +2,8 @@ package com.datn.bia.a.common.base.ext
 
 import android.content.Context
 import android.util.DisplayMetrics
+import java.text.NumberFormat
+import java.util.Locale
 
 fun Number.dpToPx(context: Context): Float {
     return this.toFloat() * (context.resources.displayMetrics.densityDpi.toFloat() / DisplayMetrics.DENSITY_DEFAULT)
@@ -9,4 +11,10 @@ fun Number.dpToPx(context: Context): Float {
 
 fun Number.pxToDp(context: Context): Float {
     return this.toFloat() / (context.resources.displayMetrics.densityDpi.toFloat() / DisplayMetrics.DENSITY_DEFAULT)
+}
+
+fun Number.formatVND(): String {
+    val localeVN = Locale("vi", "VN")
+    val formatter = NumberFormat.getCurrencyInstance(localeVN)
+    return formatter.format(this)
 }
