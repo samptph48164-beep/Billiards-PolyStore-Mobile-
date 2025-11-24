00a6dd81d5b7a47bf5ca74381dd0636c6537a58d
tranquan <tranquan12a11@gmail.com>
2025-11-24 23:47:01 +0700

    Done: Logic call api category + product, Filter Product by Category

--- app/src/main/java/com/datn/bia/a/present/fragment/home/HomeFragment.kt
+++ app/src/main/java/com/datn/bia/a/present/fragment/home/HomeFragment.kt
@@ -1,12 +1,130 @@
 package com.datn.bia.a.present.fragment.home
 
+import android.content.Intent
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.lifecycleScope
+import com.datn.bia.a.R
+import com.datn.bia.a.common.AppConst
+import com.datn.bia.a.common.UiState
 import com.datn.bia.a.common.base.BaseFragment
+import com.datn.bia.a.common.base.ext.showToastOnce
 import com.datn.bia.a.databinding.FragmentHomeBinding
+import com.datn.bia.a.present.activity.prod.ProductActivity
+import com.datn.bia.a.present.fragment.home.adapter.CatAdapter
+import com.datn.bia.a.present.fragment.home.adapter.ProductAdapter
+import com.google.gson.Gson
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
 class HomeFragment : BaseFragment<FragmentHomeBinding>() {
+
+    private val viewModel: HomeViewModel by viewModels()
+
+    private var catAdapter: CatAdapter? = null
+    private var productAdapter: ProductAdapter? = null
+    private var gson: Gson? = null
+
     override fun inflateBinding(): FragmentHomeBinding =
         FragmentHomeBinding.inflate(layoutInflater)
 
+    override fun initViews() {
+        super.initViews()
+
+        gson = Gson()
+
+        binding.rcvCategory.apply {
+            catAdapter = CatAdapter(
+                onClick = { index, cat ->
+                    catAdapter?.changeIndexSelect(index)
+                    productAdapter?.submitData(
+                        viewModel.allPro.value.filter { it.category?.id == viewModel.allCat.value[index].id }
+                    )
+                }
+            )
+
+            adapter = catAdapter
+        }
+
+        binding.rcvProduct.apply {
+            productAdapter = ProductAdapter(
+                contextParams = requireContext(),
+                onProductClick = { index, product ->
+                    gson?.toJson(product)?.let { json ->
+                        startActivity(Intent(
+                            requireContext(),
+                            ProductActivity::class.java
+                        ).apply {
+                            putExtra(AppConst.KEY_PRODUCT_DETAIL, json)
+                        })
+                    } ?: run {
+                        requireContext().showToastOnce(getString(R.string.msg_wrong))
+                    }
+                }
+            )
+
+            adapter = productAdapter
+        }
+    }
+
+    override fun observeData() {
+        super.observeData()
+
+        lifecycleScope.launch {
+            viewModel.state.collect { homeState ->
+                when (val uiState = homeState.productState) {
+                    is UiState.Error -> {
+                        requireContext().showToastOnce(uiState.message)
+                        viewModel.changeProductStateToIdle()
+                    }
+
+                    UiState.Idle -> {
+
+                    }
+
+                    UiState.Loading -> {
+
+                    }
+
+                    is UiState.Success -> {
+                        val data = uiState.data
+                        productAdapter?.submitData(data.data ?: emptyList())
+                        viewModel.cacheAllPro(data.data ?: emptyList())
+                        viewModel.changeProductStateToIdle()
+                    }
+                }
+
+                when (val uiState = homeState.catState) {
+                    is UiState.Error -> {
+                        viewModel.changeCatStateToIdle()
+                    }
+
+                    UiState.Idle -> {
+
+                    }
+
+                    UiState.Loading -> {
+
+                    }
+
+                    is UiState.Success -> {
+                        val data = uiState.data
+                        catAdapter?.submitData(data)
+                        viewModel.cacheAllCat(data)
+                        viewModel.changeCatStateToIdle()
+                    }
+                }
+            }
+        }
+    }
+
+    override fun onDestroy() {
+        catAdapter?.list?.clear()
+        catAdapter = null
+        productAdapter?.list?.clear()
+        productAdapter = null
+        gson = null
+
+        super.onDestroy()
+    }
 }
---
+++ app/src/main/java/com/datn/bia/a/present/fragment/home/HomeViewModel.kt
@@ -0,0 +1,63 @@
+package com.datn.bia.a.present.fragment.home
+
+import androidx.lifecycle.viewModelScope
+import com.datn.bia.a.common.UiState
+import com.datn.bia.a.common.base.BaseViewModel
+import com.datn.bia.a.domain.model.dto.res.ResCatDTO
+import com.datn.bia.a.domain.model.dto.res.ResProductDataDTO
+import com.datn.bia.a.domain.usecase.cat.FetchAllCatUseCase
+import com.datn.bia.a.domain.usecase.product.FetchAllProductsUseCase
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.combine
+import kotlinx.coroutines.flow.stateIn
+import javax.inject.Inject
+
+@HiltViewModel
+class HomeViewModel @Inject constructor(
+    fetchAllProductsUseCase: FetchAllProductsUseCase,
+    fetchAllCatUseCase: FetchAllCatUseCase
+) : BaseViewModel() {
+    private val _allCat = MutableStateFlow(emptyList<ResCatDTO>())
+    val allCat = _allCat.asStateFlow()
+    private val _allPro = MutableStateFlow(emptyList<ResProductDataDTO>())
+    val allPro = _allPro.asStateFlow()
+    private val _state = MutableStateFlow(HomeState())
+    val state = combine(
+        _state,
+        fetchAllProductsUseCase.invoke(),
+        fetchAllCatUseCase.invoke()
+    ) { state, productState, catState ->
+        state.copy(
+            productState = productState,
+            catState = catState
+        )
+    }.stateIn(
+        scope = viewModelScope,
+        started = SharingStarted.WhileSubscribed(
+            stopTimeoutMillis = 5000
+        ), initialValue = HomeState()
+    )
+
+    fun changeProductStateToIdle() {
+        _state.value = _state.value.copy(
+            productState = UiState.Idle
+        )
+    }
+
+    fun changeCatStateToIdle() {
+        _state.value = _state.value.copy(
+            catState = UiState.Idle
+        )
+    }
+
+    fun cacheAllCat(list: List<ResCatDTO>) {
+        _allCat.value = list
+    }
+
+    fun cacheAllPro(list: List<ResProductDataDTO>) {
+        _allPro.value = list
+    }
+}
