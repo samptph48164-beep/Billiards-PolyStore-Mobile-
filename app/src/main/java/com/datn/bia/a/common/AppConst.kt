2bdf0674f6ba4b1ec90c1f395d49cd02a3dbe033
samptph48164-beep <samptph48164@fpt.edu.vn>
2025-12-04 23:36:58 +0700

    done: add const

--- app/src/main/java/com/datn/bia/a/common/AppConst.kt
+++ app/src/main/java/com/datn/bia/a/common/AppConst.kt
@@ -2,4 +2,6 @@ package com.datn.bia.a.common
 
 object AppConst {
     internal const val KEY_PRODUCT_DETAIL = "product"
+    internal const val KEY_VOUCHER = "voucher"
+    internal const val KEY_FROM_PROFILE = "profile"
 }
