be123d67b83c134869ca32a6460bd9ffade846ea
samptph48164-beep <samptph48164@fpt.edu.vn>
2025-12-04 23:32:14 +0700

    done: change response login

--- app/src/main/java/com/datn/bia/a/domain/model/dto/res/ResLoginUserDTO.kt
+++ app/src/main/java/com/datn/bia/a/domain/model/dto/res/ResLoginUserDTO.kt
@@ -1,4 +1,22 @@
 package com.datn.bia.a.domain.model.dto.res
 
-class ResLoginUserDTO {
+import com.google.gson.annotations.SerializedName
+
+data class ResLoginUserDTO(
+    val user: DataUserLogin? = null,
+    val token: String? = null,
+    val message: String? = null,
+) {
 }
+
+data class DataUserLogin(
+    @SerializedName("_id")
+    val id: String? = null,
+    val username: String? = null,
+    val email: String? = null,
+    val password: String? = null,
+    val role: String? = null,
+    val active: String? = null,
+    val createdAt: String? = null,
+    val updatedAt: String? = null,
+)
