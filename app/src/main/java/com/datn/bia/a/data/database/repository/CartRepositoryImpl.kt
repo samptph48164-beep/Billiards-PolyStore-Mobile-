0d5f88ebb9e0b5035f5b61fdeb5a474d46a3ba11
tranquan <tranquan12a11@gmail.com>
2025-12-06 23:18:06 +0700

    done: config logic cart

--- app/src/main/java/com/datn/bia/a/data/database/repository/CartRepositoryImpl.kt
+++ app/src/main/java/com/datn/bia/a/data/database/repository/CartRepositoryImpl.kt
@@ -3,6 +3,7 @@ package com.datn.bia.a.data.database.repository
 import com.datn.bia.a.data.database.dao.CartDao
 import com.datn.bia.a.domain.model.entity.CartEntity
 import com.datn.bia.a.domain.repository.CartRepository
+import kotlinx.coroutines.flow.Flow
 import javax.inject.Inject
 
 class CartRepositoryImpl @Inject constructor(
@@ -16,4 +17,12 @@ class CartRepositoryImpl @Inject constructor(
 
     override suspend fun updateCart(c: CartEntity) =
         cartDao.updateCart(c)
+
+    override fun getAllCartEnable(): Flow<List<CartEntity>> =
+        cartDao.getAllCartsEnable()
+
+    override suspend fun searchCartEnableByIdCart(id: Long): CartEntity? =
+        cartDao.searchCartEnableByIdCart(id)
+
+    override suspend fun deleteCart(c: CartEntity) = cartDao.deleteCart(c)
 }
